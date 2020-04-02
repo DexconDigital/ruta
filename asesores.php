a60f8089c6ef7c9c209f74be7c28775cdc4f3b0c
sanzmanu <53055872+sanzmanu@users.noreply.github.com>
2020-04-02 13:40:55 -0500

    x

--- asesores.php
+++ asesores.php
@@ -27,19 +27,19 @@
 <!-- contenedor asesor -->
 <li class="col-md-3 col-sm-4 col-xs-6">
     <div class="team" itemscope itemtype="http://schema.org/Person">
-        <img width="270" itemprop="image" height="288" src="imagenesAsesores/Melisa Salzar.png" class="img-responsive wp-post-image"
+        <img width="270" itemprop="image" height="288" src="imagenesAsesores/cleiber.jpg" class="img-responsive wp-post-image"
          alt="" />
         <div class="team-over">
             <!-- Iconos que aparecen en el hover del asesor -->
             <ul class="social_icons animated-6s fadeInUp">
                 <li class="facebook"><a itemprop="telephone" href="tel:0344484667" target="_blank"><i class="fa fa-phone"></i></a></li>
-                <li class="facebook"><a href="https://wa.me/573183543831" target="_blank"><i class="fab fa-whatsapp"></i></a></li>
+                <li class="facebook"><a href="https://wa.me/573176482160" target="_blank"><i class="fab fa-whatsapp"></i></a></li>
             </ul>
             <!-- fin de iconos que aparecen en el hover -->
         </div>
         <!-- informacion del asesor -->
         <div class="team-detail">
-            <h6><a itemprop="name" href="#">Melisa Salazar Ospina</a></h6>
+            <h6><a itemprop="name" href="#">Cleiber Cuervo Granada</a></h6>
             <p itemprop="jobTitle">Asesor Comercial</p>
         </div>
         <!-- fin de informacion del asesor -->
@@ -50,21 +50,21 @@
 <!-- contenedor asesor -->
 <li class="col-md-3 col-sm-4 col-xs-6">
     <div class="team" itemscope itemtype="http://schema.org/Person">
-        <img itemprop="image" width="270" height="288" src="imagenesAsesores/Sandra Correa (2).png" class="img-responsive wp-post-image"
+        <img itemprop="image" width="270" height="288" src="imagenesAsesores/lina.jpg" class="img-responsive wp-post-image"
          alt="" />
         <div class="team-over">
             <!-- iconos que aparecen en el hover -->
             <ul class="social_icons animated-6s fadeInUp">
                 <li class="facebook"><a itemprop="telephone" href="tel:0344484667" target="_blank"><i class="fa fa-phone"></i></a></li>
-                <li class="facebook"><a href="https://wa.me/573176482160" target="_blank"><i class="fab fa-whatsapp"></i></a></li>
+                <li class="facebook"><a href="https://wa.me/573183543831" target="_blank"><i class="fab fa-whatsapp"></i></a></li>
             </ul>
             <!-- fin de iconos que aparecen en el hover -->
         </div>
 
         <!-- detalles del asesor -->
         <div class="team-detail">
-            <h6><a itemprop="name" href="#">Sandra Correa Jaramillo</a></h6>
-            <p itemprop="jobTitle">Coordinadora Comercial</p>
+            <h6><a itemprop="name" href="#">Lina María Ramírez Zapata</a></h6>
+            <p itemprop="jobTitle">Coordinadora Comercial y de Mercadeo</p>
         </div>
         <!-- fin de detalles del asesor -->
     </div>
